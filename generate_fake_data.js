a9feb7ac2be6bd3dd1e6a9aa62314e5627c69866
Jeff <625585@bah.com>
2023-05-03 10:54:06 -0400

    progress bar

--- generate_fake_data.js
+++ generate_fake_data.js
@@ -5,6 +5,7 @@
 // Step 2: Parse command line arguments using yargs
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
+const ProgressBar = require('progress');
 const argv = yargs(hideBin(process.argv))
   .option('n', {
     alias: 'rows',
@@ -24,6 +25,15 @@ const argv = yargs(hideBin(process.argv))
     type: 'string',
     demandOption: true
   }).argv;
+  
+// Add the following lines after parsing the command line arguments
+// Step 3: Create a progress bar instance with a custom format
+const progressBar = new ProgressBar('Generating data [:bar] :percent :etas', {
+  complete: '=',
+  incomplete: ' ',
+  width: 20,
+  total: argv.rows
+});
 
 // Step 3: Read the input XLSX file using SheetJS
 const XLSX = require('xlsx');
@@ -39,7 +49,8 @@ for (let col = range.s.c; col <= range.e.c; ++col) {
   headers.push(cell ? cell.v : '');
 }
 
-// Step 5: Generate the specified number of rows of fake data with random strings
+// Modify the data generation loop in Step 5
+// Step 4: Update the progress bar while generating fake data
 const data = [];
 for (let i = 0; i < argv.rows; i++) {
   const row = [];
@@ -47,6 +58,7 @@ for (let i = 0; i < argv.rows; i++) {
     row.push(Math.random().toString(36).slice(-10));
   }
   data.push(row);
+  progressBar.tick(); // Update the progress bar
 }
 
 // Step 6: Create a new XLSX file with the generated data
@@ -56,3 +68,4 @@ XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, sheetName);
 
 // Step 7: Save the output XLSX file to the specified path
 XLSX.writeFile(newWorkbook, argv.output);
+progressBar.terminate();
